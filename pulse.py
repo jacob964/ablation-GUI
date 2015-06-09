4edd4121b7668116b742c4b20898919e2da643be
jacob964 <jalbrit11@gmail.com>
2015-06-08 20:34:15 -0600

    Made pulse.py compatible with brush cleaning step

--- pulse.py
+++ pulse.py
@@ -24,6 +24,29 @@ hex_pack	= config.hexLength
 
 # Other Parameters
 relative   	= config.relative
+y_brush_top = 335
+y_brush_dist = 80
+x_brush = 232
+cleanTrigger = 10
+
+x_current = x_start
+y_current = y_start
+
+def currentX(x):
+	global x_current
+	x_current += x
+	return x_current
+	
+def currentY(y):
+	global y_current
+	y_current += y
+	return y_current
+	
+
+def check_Conditions(laser, dwell, z):
+	if len(laser) != len(dwell): return 0
+	if len(laser) != len(z): return 0
+	return len(laser)
 
 def hex_pattern(hex_pack):
 	global x_dist
@@ -81,16 +104,47 @@ def gcode_move(x_move, y_move):
 	y_move = formatFloat(y_move)
 	f.writelines("G0 X" + str(x_move) + " Y" + str(y_move) + " F1000\n")
 	f.writelines("G4 P" + str(pauseTime) + "\n")
+	currentX(x_move)
+	currentY(y_move)
 
+def cleanLaserHead():
+	x_initial = currentX(0)
+	y_initial = currentY(0)
+	
+	f.writelines("\n;; Cleaning\n\n")
+	f.writelines("M3 S0\n")
+	
+	f.writelines("G90\n")
+	f.writelines("G0 X" + str(x_brush) + " Y" + str(y_brush_top) + " F1000\n")
+	f.writelines("G4 P250\n")
+	f.writelines("G0 Y" + str(y_brush_top - y_brush_dist) + " F1000\n")
+	f.writelines("G4 P250\n")
+	f.writelines("G0 Y" + str(y_brush_top) + " F1000\n")
+	f.writelines("G4 P250\n")
+	f.writelines("G0 X" + str(x_initial) + " Y" + str(y_initial) + " F1000\n")
+	f.writelines("G4 P500\n")
+	f.writelines("G91\n")
+	
+	f.writelines(";; Done Cleaning\n\n")
+	
+	
 def gcode_rectangle(dwellTime, laserPower):
 	x_total = 0
 	y_total = 0
 	flag = -1
 	pulseDist = formatFloat(1/x_dist)
+	
+	cleanCount = 0
+	
 	while y_total < rectWidth:
 		f.writelines("G1 X" + str(-1*rectLength) + " S" + str(laserPower) + " L" + str(dwellTime*1000) + " P" + str(pulseDist) + " F" + str(feedRate) + " B1\n") 
-		#f.writelines("M3 S0\n")
+		currentX(-1*rectLength)
 		x_total -= rectLength
+		
+		cleanCount += 1
+		if cleanCount == cleanTrigger:
+			cleanCount = 0 
+			cleanLaserHead()
 		
 		x_overhang = flag * x_dist/2
 		gcode_move(rectLength + x_overhang, -1*y_dist)
@@ -140,6 +194,8 @@ def writeGCODE(kvarg):
 	gcode_move(x_move + x_grid, y_move - rectWidth - spaceSmall)
 	x_grid = 0
 	y_grid += rectWidth + spaceSmall
+
+	cleanLaserHead()	
 	
 	writefile.closefile(f)
 
